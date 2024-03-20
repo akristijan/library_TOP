712ee6be18dfaa809909d5ea7b7f4a6a9399fbb0
Kristijan <kristijan.aleksic@gmail>
2024-03-20 22:38:21 +0100

    functions added

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const submitBtn = document.querySelector('.btn-submit')
 const closeBtn = document.querySelector('.btn-close')
 
 
-let myLibrary = [];
+let myLibrary = [
+    {"title" : "Lov na vjestice", "author": "J.K. Kotlin", "pages": 123, "read":"notread"}, 
+    {"title" : "Vlak u snijegu", "author": "Orman Lokus", "pages": 223, "read":"notread"}, 
+
+];
 
 function Book(title, author, pages, read) {
     
@@ -27,3 +31,12 @@ addBookBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     dialog.close();
 })
+
+// Library functions 
+// Save book in library
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+  }
+
+
+
